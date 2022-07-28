c2977be6ef98fbfd34babd1c9e30f229b47f3d2f
tjswnd2a <tjswnd2a@naver.com>
2022-07-29 01:41:07 +0900

    20220729

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-const searchEl = dcument.querySelector(".search");
-const inputEl = searchEl.querySelector("input");
+const searchEl = document.querySelector(".search");
+const search_inputEl = searchEl.querySelector(".search_text");
 
 searchEl.addEventListener('click', function(){
-  
-})
+  search_inputEl.focus();
+});
+
+search_inputEl.addEventListener('focus', function() {
+  search_inputEl.classList.add('focus');
+  search_inputEl.setAttribute('placeholder', '통합검색');
+});
+
+search_inputEl.addEventListener('blur', function() {
+  search_inputEl.classList.remove('focus');
+  search_inputEl.setAttribute('placeholder','');
+});
