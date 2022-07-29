f1374af2aaf0ec791629023ab37ff34354090a12
tjswnd2a <tjswnd2a@naver.com>
2022-07-29 18:08:45 +0900

    20220729(2)

--- js/main.js
+++ js/main.js
@@ -14,3 +14,25 @@ search_inputEl.addEventListener('blur', function() {
   search_inputEl.classList.remove('focus');
   search_inputEl.setAttribute('placeholder','');
 });
+
+const arrow_toggleBtn = document.querySelector('.arrow_toggle');
+const arrow_downEl = document.querySelector('.arrow_toggle .arrow_circle_down');
+const arrow_upEl = document.querySelector('.arrow_toggle .arrow_circle_up');
+
+const promotionEl = document.querySelector(".promotion");
+let HidePromotion = false;
+
+arrow_toggleBtn.addEventListener('click', function(){
+  console.log('aaaaa');
+  HidePromotion = !HidePromotion;
+  if( HidePromotion === true ){
+    arrow_downEl.classList.add('invisible');
+    arrow_upEl.classList.remove('invisible');
+    promotionEl.classList.add('visible');
+  }else{
+    arrow_upEl.classList.add('invisible');
+    arrow_downEl.classList.remove('invisible');
+    promotionEl.classList.remove('visible');
+
+  }
+})
